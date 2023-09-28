106742f6faf3e93dfcc999b6ae092b54af58bc91
Mjit19 <mjitmukherjee19@gmail.com>
2023-09-28 11:38:18 +0530

    Add feature for playing out a round

--- script.js
+++ script.js
@@ -28,3 +28,40 @@ function playerSelection() {
     return pick;
 }
 
+// Play a round, print round result
+function playRound(playerSelection, computerSelection) {
+
+    //   If the values match, draw and returns result at the end
+    let result = "It's a draw!";
+
+//   If the values don't match, decide who won and who lost
+    if (playerSelection !== computerSelection){
+        if (computerSelection === "rock") {
+            if (playerSelection === "paper") {
+                result = "You Win! Paper beats Rock"
+            } else {
+                result = "You Lose! Rock beats Scissors"
+            }
+        } else if (computerSelection === "paper") {
+            if (playerSelection === "scissors") {
+                result = "You Win! Scissors beats Paper"
+            } else {
+                result = "You Lose! Paper beats Rock"
+            }
+        } else {
+            if (playerSelection === "rock") {
+                result = "You Win! Rock beats Scissors"
+            } else {
+                result = "You Lose! Scissors beats Paper"
+            }
+        }
+    }
+
+    return result;
+}
+// play game, print game result at the end
+
+//   Play 5 rounds
+
+
+
